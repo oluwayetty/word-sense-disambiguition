416729c4aa6328473673a573152df8db6135d94f
Oluwayetty <oluwayetti@gmail.com>
2019-05-24 04:52:20 +0200

    re-ordered folder structure

--- code/model.py
+++ code/model.py
@@ -1,16 +1,27 @@
-from config import DATA_FOLDER, EMBEDDINGS
-from utils import return_data_for_cbow
+from config import DATA_FILE, EMBEDDINGS, GZ_DATA
+from utils import return_data_for_cbow, return_line_sentence_data_format, return_gz_format
 from gensim.models import Word2Vec
 
 
 def train_cbow_model(training_data):
     print("Training started.....")
-    model = Word2Vec(training_data, min_count=1, ns_exponent=10e-3, window=4, sg=0, size=1)
+    model = Word2Vec(sentences = training_data, min_count=1, ns_exponent=10e-3, window=4, sg=0, size=1)
     model.wv.save_word2vec_format(EMBEDDINGS)
 
+
+def train_cbow_model_line_sentence(training_data):
+    print("Training started.....")
+    model = Word2Vec(corpus_file = training_data, min_count=1, ns_exponent=10e-3, window=4, sg=0, size=1)
+    model.wv.save_word2vec_format(EMBEDDINGS)
+
+
 def main():
-    training_data = return_data_for_cbow(DATA_FOLDER+ '/training.txt')
+    training_data = return_data_for_cbow(DATA_FILE)
     train_cbow_model(training_data)
+
+    # training_data = return_gz_format(DATA_FILE)
+    # train_cbow_model_line_sentence(training_data)
+
     print("Training done, check "+ EMBEDDINGS + " for the embeddings")
 
 if __name__ == '__main__':
--- code/utils.py
+++ code/utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from config import DATA_FOLDER
+from config import DATA_FOLDER, GZ_DATA
+import gzip
 
 def return_data_for_cbow(filepath):
     data = pd.read_csv(filepath)
@@ -17,3 +18,26 @@ def return_data_for_cbow(filepath):
     splitted = [i.split() for i in data]
 
     return splitted
+
+def return_line_sentence_data_format(filepath):
+    data = pd.read_csv(filepath)
+
+    # drop duplicated lines
+    data = data.drop_duplicates(['sentence'], keep='last')
+
+    # drop any empty row
+    data = data.dropna()
+
+    return data
+
+
+def return_gz_format(filepath):
+    fp = open(filepath, "rb")
+    data = fp.read()
+    bindata = bytearray(data)
+    with gzip.open(GZ_DATA, "wb") as f:
+        f.write(bindata)
+    return GZ_DATA
+
+def remove_context_words(filepath):
+    return ""
